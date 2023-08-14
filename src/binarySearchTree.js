308323a7894ab1d695188530a8500c7c1d6b1fe7
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-14 19:58:15 +0100

    Import MergeSort And Build BinarySearchTree.js

---
+++ src/binarySearchTree.js
@@ -0,0 +1,8 @@
+import { mergeSort } from "./mergeSort";
+
+export class Tree {
+    constructor(array) {
+        this.array = array;
+        this.root = null;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { Tree } from "./binarySearchTree";
+
+
+
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+const newTree = new Tree(testArray)
+
+console.log('h')
