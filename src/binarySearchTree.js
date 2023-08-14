4beeb6237680e589c7cfdfdd6ea04ea97fe90500
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-14 23:19:55 +0100

    Sort And Remove Duplicates From Array
    The array for building a balanced binary search tree is sorted using
    merge sort, and to remove duplicate values from the array, the array is
    converted into a Set and then back to an array.

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -5,4 +5,28 @@ export class Tree {
         this.array = array;
         this.root = null;
     }
+    buildTree() {
+        let sortedArray = mergeSort(this.array)
+        let removedDuplicates = [... new Set(sortedArray)]
+
+        this.recursiveBuildTree(sortedArray)
+
+
+
+    }
+
+    recursiveBuildTree(array) {
+        // if (array.length === 0) return array;
+        // else {
+            const middleIndex = Math.ceil(0 + array.length)/2
+            const root = array[middleIndex]
+            const leftHalf = array.slice(0, middleIndex)
+            const rightHalf = array.slice(middleIndex + 1) // don't include the root (middle)
+
+            console.log(root)
+            console.log(leftHalf)
+            console.log(rightHalf)
+            // return this.recursiveBuildTree()
+        // }
+    }
 }
