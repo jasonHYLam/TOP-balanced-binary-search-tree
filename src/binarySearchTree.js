f83f22927fa23ff64fd23f473285e281b2571f26
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-15 23:04:34 +0100

    Refactor BuildTree() To Use Tree Attributes
    BuildTree() is refactored to use tree.root, tree.leftChildren,
    tree.rightChildren, and tree.array.

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -8,29 +8,26 @@ class Tree {
         this.rightChildren = null;
     }
 }
-    function buildTree() {
+export function buildTree(array) {
         let sortedArray = mergeSort(array)
         let removedDuplicates = [... new Set(sortedArray)]
 
+        console.log(removedDuplicates)
         recursiveBuildTree(removedDuplicates)
     }
 
-    function recursiveBuildTree(array) {
-        if (array.length === 0) return array;
-        // else {
-            const tree = newTree(array)
-            const middleIndex = Math.floor(array.length/2)
-            tree.root = array[middleIndex]
+function recursiveBuildTree(array) {
+    if (array.length === 0) return array;
+    // else {
+        const tree = new Tree(array)
+        const middleIndex = Math.floor(array.length/2)
+        tree.root = array[middleIndex]
             
-            const leftHalf = array.slice(0, middleIndex)
-            const rightHalf = array.slice(middleIndex + 1) // don't include the root (middle)
+        tree.leftChildren = tree.array.slice(0, middleIndex)
+        tree.rightChildren = tree.array.slice(middleIndex + 1)
 
-            console.log(root)
-            recursiveBuildTree(leftHalf)
-            console.log(leftHalf)
-            recursiveBuildTree(rightHalf)
-            console.log(rightHalf)
-            return root;
-            // not exactly sure what is happening here
-        // }
-    }
+        console.log(tree.root)
+        recursiveBuildTree(tree.leftChildren)
+        recursiveBuildTree(tree.rightChildren)
+        return tree.root;
+}
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import { buildTree } from "./binarySearchTree";
 
 const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
+console.log('h')
 buildTree(testArray)
